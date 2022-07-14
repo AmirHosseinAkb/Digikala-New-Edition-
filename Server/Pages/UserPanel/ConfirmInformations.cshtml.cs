2302cdc46a0cadbb27e67e0ab3e7364c6e21ae97
AmirHosseinAkb <amirbvk1@gmail.com>
2022-07-15 00:53:55 +0430

    ConfirmUserInformationsUI

---
+++ Server/Pages/UserPanel/ConfirmInformations.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Server.Pages.UserPanel
+{
+    public class ConfirmInformationsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
