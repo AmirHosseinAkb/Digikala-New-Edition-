a4c56e7e55ba24cbddccf7409bf9ef354731a29c
AmirHosseinAkb <amirbvk1@gmail.com>
2022-07-19 19:19:39 +0430

    AddTransactionRepository

---
+++ UserManagement.Infrastructure.EfCore/Repositories/TransactionRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserManagement.Domain.Transaction;
+
+namespace UserManagement.Infrastructure.EfCore.Repositories
+{
+    public class TransactionRepository:ITransactionRepository
+    {
+    }
+}
