1c7928a4ecbad327121a548b642caa03b87be321
AmirHosseinAkb <amirbvk1@gmail.com>
2022-07-21 18:44:19 +0430

    ConfigAdministrationLayout

---
+++ Server/Areas/Administration/Pages/Users/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Server.Areas.Administration.Pages.Users
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
