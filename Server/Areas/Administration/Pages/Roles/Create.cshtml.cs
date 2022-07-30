3097a559b9118190c69844aae1064ee4093318d4
AmirHosseinAkb <amirbvk1@gmail.com>
2022-07-30 23:21:13 +0430

    ConfirmCreateRolePage

---
+++ Server/Areas/Administration/Pages/Roles/Create.cshtml.cs
@@ -0,0 +1,64 @@
+using _01_Framework.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using UserManagement.Application.Contracts.Role;
+
+namespace Server.Areas.Administration.Pages.Roles
+{
+    public class CreateModel : PageModel
+    {
+        public CreateModel(IRoleApplication roleApplication,IEnumerable<IPermissionExposer> exposers)
+        {
+            _roleApplication=roleApplication;
+            _exposers=exposers;
+        }
+        private readonly IRoleApplication _roleApplication;
+
+        private readonly IEnumerable<IPermissionExposer> _exposers;
+
+        public string ErrorMessage { get; set; }
+
+        [BindProperty]
+        public CreateRoleCommand Command { get; set; }
+
+        public List<SelectListItem> Permissions { get; set; } = new List<SelectListItem>();
+
+        public void GetInformations()
+        {
+            foreach (var exposer in _exposers)
+            {
+                var exposedPermissions = exposer.Expose();
+                foreach (var (key,value) in exposedPermissions)
+                {
+                    foreach (var permission in value)
+                    {
+                        Permissions.Add( new SelectListItem(permission.PermissionTitle, permission.PermissionCode.ToString())
+                        {
+                            Group = new SelectListGroup(){Name = key}
+                        });
+                    }
+                }
+            }
+        }
+        public void OnGet()
+        {
+            GetInformations();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+                return RedirectToPage();
+            
+            var result=_roleApplication.Create(Command);
+            if (!result.IsSucceeded)
+            {
+                ErrorMessage = result.Message;
+                GetInformations();
+                return Page();
+            }
+            return RedirectToPage("Index");
+        }
+    }
+}
--- Server/Areas/Administration/Pages/Roles/Index.cshtml.cs
+++ Server/Areas/Administration/Pages/Roles/Index.cshtml.cs
@@ -9,46 +9,21 @@ namespace Server.Areas.Administration.Pages.Roles
     public class IndexModel : PageModel
     {
 
-        public IndexModel(IRoleApplication roleApplication,IEnumerable<IPermissionExposer> exposers)
+        public IndexModel(IRoleApplication roleApplication)
         {
             _roleApplication = roleApplication;
-            _exposers = exposers;
         }
         private readonly IRoleApplication _roleApplication;
 
-        private readonly IEnumerable<IPermissionExposer> _exposers;
 
         public List<RoleViewModel> RoleVm { get; set; }
 
-        public List<SelectListItem> Permissions { get; set; } = new List<SelectListItem>();
-        [BindProperty]
-        public CreateRoleCommand Command { get; set; }
+
         public void OnGet()
         {
-            foreach (var exposer in _exposers)
-            {
-                var exposedPermissions = exposer.Expose();
-                foreach (var (key,value) in exposedPermissions)
-                {
-                    foreach (var permission in value)
-                    {
-                        Permissions.Add( new SelectListItem(permission.PermissionTitle, permission.PermissionCode.ToString())
-                        {
-                            Group = new SelectListGroup(){Name = key}
-                        });
-                    }
-                }
-            }
             RoleVm = _roleApplication.GetRoles();
         }
 
-        public IActionResult OnPostCreateRole()
-        {
-            if (!ModelState.IsValid)
-                return RedirectToPage();
-
-            _roleApplication.Create(Command);
-            return RedirectToPage();
-        }
+       
     }
 }
