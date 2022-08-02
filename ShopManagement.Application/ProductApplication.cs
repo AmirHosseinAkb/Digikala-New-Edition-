213af04262a6e83433b0704675b8ce3de6ce658b
AmirHosseinAkb <amirbvk1@gmail.com>
2022-08-02 23:03:17 +0430

    AddProductApplication

---
+++ ShopManagement.Application/ProductApplication.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShopManagement.Application.Contracts.Product;
+
+namespace ShopManagement.Application
+{
+    public class ProductApplication:IProductApplication
+    {
+    }
+}
